f6b9145f6908ec54b9f919ed2a4c1b1b0db42450
Paulo Augusto Silva Miranda <augustosilvamiranda8@gmail.com>
2025-10-28 17:02:30 -0300

    Iniciando Ui com Swing, tela principal + acesso feito

---
+++ src/main/java/com/br/pdvpostocombustivel/view/TelaPrincipalSw.java
@@ -0,0 +1,15 @@
+package com.br.pdvpostocombustivel.view;
+
+import javax.swing.*;
+
+public class TelaPrincipalSw {
+    private JPanel TelaPrincipal;
+    private JLabel selecioneAClasseLabel;
+    private JButton seleAcesso;
+    private JButton seleContato;
+    private JButton seleEstoque;
+    private JButton seleCusto;
+    private JButton selePessoa;
+    private JButton selePreco;
+    private JButton seleProduto;
+}
---
+++ src/main/java/com/br/pdvpostocombustivel/view/acesso/AcessoSw.java
@@ -0,0 +1,13 @@
+package com.br.pdvpostocombustivel.view.acesso;
+
+import javax.swing.*;
+
+public class AcessoSw {
+    private JTable table1;
+    private JButton addBut;
+    private JButton editBut;
+    private JButton apagBut;
+    private JTextField campID;
+    private JTextField campUSUARIO;
+    private JTextField campSENHA;
+}
