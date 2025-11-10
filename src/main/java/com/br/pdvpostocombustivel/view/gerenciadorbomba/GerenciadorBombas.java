bfc9e081e067e2befa3b508a0ea31541c4396539
Paulo Augusto Silva Miranda <augustosilvamiranda8@gmail.com>
2025-11-10 18:07:46 -0300

    interface do gerenciador de bombas

--- src/main/java/com/br/pdvpostocombustivel/view/gerenciadorbomba/GerenciadorBombas.java
+++ src/main/java/com/br/pdvpostocombustivel/view/gerenciadorbomba/GerenciadorBombas.java
@@ -4,4 +4,26 @@ import javax.swing.*;
 
 public class GerenciadorBombas {
     private JPanel panel1;
+    private JComboBox PagB2;
+    private JComboBox PagB1;
+    private JComboBox PagB3;
+    private JComboBox comboBox1;
+    private JButton EFETUARPAGAMENTOButton;
+    private JButton GERARCOMPROVANTEButton;
+    private JButton INICIARATENDIMENTOButton;
+    private JLabel statusB1;
+    private JLabel statusB2;
+    private JLabel statusB3;
+    private JLabel preçoB3;
+    private JLabel preçoB2;
+    private JLabel preçoB1;
+    private JTextField quantB1;
+    private JTextField quantB2;
+    private JTextField quantB3;
+    private JLabel preçoTotalB1;
+    private JPanel B1;
+    private JPanel B3;
+    private JPanel B2;
+    private JLabel preçoTotalB2;
+    private JLabel PreçoTotalB3;
 }
